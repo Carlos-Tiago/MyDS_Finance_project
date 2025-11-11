72423d35320755b8e852a6b3d76c842753f405b3
Carlos-Tiago <carlostiago.arinto@gmail.com>
2025-11-11 22:30:24 +0000

    The feature 'year_month' is now treated as a date (Timestamp) rather than a string.

--- code/data_explorer_aux_methods.py
+++ code/data_explorer_aux_methods.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def time_frame_plot_etfs(df_etfs, eu_stock_symbols, suptitle_part):
+def time_frame_plot_etfs(df_etfs, stock_symbols, suptitle_part):
     time_frames = ['year_month', 'month'] 
     titles = ['Year/month', 'Month']
     nticks = {'year_month': 15}
@@ -13,18 +13,25 @@ def time_frame_plot_etfs(df_etfs, eu_stock_symbols, suptitle_part):
     fig.suptitle(t=f'{suptitle_part} F.T. ETFs\' opening prices', fontsize=12)
     for ax_index, time_frame in enumerate(time_frames):
         # df open mean
-        df_om = df_etfs[df_etfs['stock_symbol'].isin(eu_stock_symbols)].copy(deep=True)
-        # Add year/month column only if needed to improve performance
-        if time_frame == 'year_month':
-            df_om['year_month'] = df_om['year'].astype('string') + '-' + df_om['month'].astype('string')
+        df_om = df_etfs[df_etfs['stock_symbol'].isin(stock_symbols)].copy(deep=True)      
         df_om = df_om.groupby(
                 by=[time_frame, 'stock_symbol'], as_index=False
-            )['open'].mean().rename(columns={'open':'open_mean'})
+            )['open'].mean().rename(columns={'open':'open_mean'})#
         
-        plot_op = sns.lineplot(data=df_om, x=df_om[time_frame], y=df_om['open_mean'],
-                                hue=df_om['stock_symbol'], legend='auto', ax=axes[ax_index]
+        
+        # Since there is not yeaar-month representation while keeping
+        # the datetime (timestamp(?)) type, use strftime to get xtick_label 
+        if time_frame == 'year_month':
+            x_s = df_om[time_frame].dt.strftime('%Y-%m')
+        else:
+            x_s = df_om[time_frame]
+        plot_op = sns.lineplot(data=df_om, x=x_s, y=df_om['open_mean'],
+                               hue=df_om['stock_symbol'], 
+                               hue_order=df_om['stock_symbol'].copy().drop_duplicates().sort_values(),
+                               legend='auto', ax=axes[ax_index]
                               )
-        xtick_labels = plot_op.get_xticklabels()
+        
+        xtick_labels = plot_op.get_xticklabels() #unique x labels
         displayed_xtick_labels = []
         if len(xtick_labels) > 15:
             step = int(np.floor(len(xtick_labels) / nticks[time_frame]))
@@ -33,8 +40,8 @@ def time_frame_plot_etfs(df_etfs, eu_stock_symbols, suptitle_part):
         
         plot_op.set( # Note that the .set method is possible since sns.lineplot() 
                      # returns a matplotlib.axes.Axes
-                title= titles[ax_index],
+                title=titles[ax_index],
                 xlabel=time_frame, ylabel='Avg open price'
             )
         if time_frame == 'year_month':
-             plot_op.set_xticks(ticks=range(0, len(xtick_labels), step), labels=displayed_xtick_labels, rotation=70)
+            plot_op.set_xticks(ticks=range(0, len(xtick_labels), step), labels=displayed_xtick_labels, rotation=70)
